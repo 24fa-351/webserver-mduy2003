db5047b5545b1353decf67aa4ee8b7e8791e1143
mduy2003 <mduy2003@csu.fullerton.edu>
2024-11-10 19:31:07 -0800

    Finished

--- server.h
+++ server.h
@@ -4,6 +4,11 @@
 #include <stddef.h>
 
 #define BUFFER_SIZE 1024
+#define METHOD_SIZE 16
+#define PATH_SIZE 256
+#define PROTOCOL_SIZE 16
+#define STATIC_LEN 7
+#define CALC_LEN 5
 
 // Global variables for handle_stats
 extern int request_count; 
@@ -11,7 +16,7 @@ extern size_t bytes_received;
 extern size_t bytes_sent;
 
 void send_response(int client_fd, const char *header, const char *content_type, const char *body, size_t body_len);
-void handle_static(int client_fd, const char *filepath);
+void handle_static(int client_fd, const char *file_path);
 void handle_stats(int client_fd);
 void handle_calc(int client_fd, const char *query);
 void *handle_connection(void *client);
