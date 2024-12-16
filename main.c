7a915177aa63963963ea2533a037332ddff81a79
mduy2003 <mduy2003@csu.fullerton.edu>
2024-12-16 00:53:12 -0800

    Reformatted

--- main.c
+++ main.c
@@ -1,33 +1,29 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#include <pthread.h>
 
 #include "server.h"
 
 #define DEFAULT_PORT 80
 #define LISTEN_BACKLOG 5
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     int port = DEFAULT_PORT;
-    if (argc == 3 && strcmp(argv[1], "-p") == 0)
-    {
+    if (argc == 3 && strcmp(argv[1], "-p") == 0) {
         port = atoi(argv[2]);
-    }
-    else if (argc != 1)
-    {
+    } else if (argc != 1) {
         fprintf(stderr, "Usage: %s -p <port>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
     int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (socket_fd < 0)
-    {
+    if (socket_fd < 0) {
         perror("Socket creation failed");
         exit(EXIT_FAILURE);
     }
@@ -38,15 +34,15 @@ int main(int argc, char *argv[])
     socket_address.sin_addr.s_addr = htonl(INADDR_ANY);
     socket_address.sin_port = htons(port);
 
-    if (bind(socket_fd, (struct sockaddr *)&socket_address, sizeof(socket_address)) < 0)
-    {
+    if (bind(socket_fd, (struct sockaddr*)&socket_address,
+            sizeof(socket_address))
+        < 0) {
         perror("Bind failed");
         close(socket_fd);
         exit(EXIT_FAILURE);
     }
 
-    if (listen(socket_fd, LISTEN_BACKLOG) < 0)
-    {
+    if (listen(socket_fd, LISTEN_BACKLOG) < 0) {
         perror("Listen failed");
         close(socket_fd);
         exit(EXIT_FAILURE);
@@ -55,15 +51,14 @@ int main(int argc, char *argv[])
     printf("Port: %d\n", port);
     printf("Command format: GET <command> HTTP/1.1\n");
 
-    while (1)
-    {
+    while (1) {
         struct sockaddr_in client_address;
         socklen_t client_address_len = sizeof(client_address);
 
-        int *client_fd = malloc(sizeof(int));
-        *client_fd = accept(socket_fd, (struct sockaddr *)&client_address, &client_address_len);
-        if (*client_fd < 0)
-        {
+        int* client_fd = malloc(sizeof(int));
+        *client_fd = accept(
+            socket_fd, (struct sockaddr*)&client_address, &client_address_len);
+        if (*client_fd < 0) {
             perror("Accept failed");
             free(client_fd);
             continue;
--- server.h
+++ server.h
@@ -11,14 +11,15 @@
 #define CALC_LEN 5
 
 // Global variables for handle_stats
-extern int request_count; 
-extern size_t bytes_received; 
+extern int request_count;
+extern size_t bytes_received;
 extern size_t bytes_sent;
 
-void send_response(int client_fd, const char *header, const char *content_type, const char *body, size_t body_len);
-void handle_static(int client_fd, const char *file_path);
+void send_response(int client_fd, const char* header, const char* content_type,
+    const char* body, size_t body_len);
+void handle_static(int client_fd, const char* file_path);
 void handle_stats(int client_fd);
-void handle_calc(int client_fd, const char *query);
-void *handle_connection(void *client);
+void handle_calc(int client_fd, const char* query);
+void* handle_connection(void* client);
 
 #endif // SERVER_H
